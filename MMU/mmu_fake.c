4e1c73e6f8453d144fc5d222c9925a6c98693fa5
SamueleAs <samuelsondf@gmail.com>
2023-12-29 01:06:18 +0100

    manca la exception e i test in memoria da fare, però almeno non ci sono piu errori dopo il make

--- MMU/mmu_fake.c
+++ MMU/mmu_fake.c
@@ -3,8 +3,9 @@
 //QUI DEVO FARE UN MMU E LE FUNZIONI 
 
 void MMU_writebyte(MMU* mmu , int pos , char c){
-    int numero_pagina= pos/page_size;
-    int offset = pos % page_size;    //formule del prof per capire pagina e offset 
+    int numero_pagina= pos/pag_size;
+    int offset = pos % pag_size;    //formule del prof per capire pagina e offset 
+    
 	
 	//controlliamo la pagina
     if (mmu->page_table[numero_pagina].valid == 0){
@@ -15,14 +16,14 @@ void MMU_writebyte(MMU* mmu , int pos , char c){
     //ora possiamo scrivere
     int num_frame = mmu->page_table[numero_pagina].num_frame;
     //portiamolo in memoria fisica
-    int pos_mem_fisica = num_frame*numero_pagina;
-    mmu->page_table[pos_mem_fisica]=c;
+    int pos_mem_fisica = (num_frame * numero_pagina) + offset ;
+    mmu->memoria_fisica[pos_mem_fisica]= c;
     mmu->page_table[numero_pagina].write=1; 
 }
 
 char MMU_readByte(MMU *mmu, int pos){
-    int numero_pagina= pos/page_size;
-    int offset = pos % page_size;    //formule del prof per capire pagina e offset 
+    int numero_pagina= pos/pag_size;
+    int offset = pos % pag_size;    //formule del prof per capire pagina e offset 
     
     //controlliamo la pagina
     if (mmu->page_table[numero_pagina].valid == 0){
@@ -30,11 +31,24 @@ char MMU_readByte(MMU *mmu, int pos){
         MMU_exception(mmu, pos); //chiamo la funzione per le eccezzioni
     }
     
+    int num_frame = mmu->page_table[numero_pagina].num_frame;
+    mmu->page_table[numero_pagina].read = 1;
+    int pos_mem_fisica = (num_frame * numero_pagina) + offset;
+    char numero= mmu-> memoria_fisica[pos_mem_fisica];
+    
+    return numero; }
     
     
-
-
+void MMU_exception(MMU* mmu, int pos){
 
 }
     
-}
+    
+    
+    
+    
+    
+    
+    
+    
+
